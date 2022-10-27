af66b3b1e39b2b6493bef680a6b5cb5c42a1c551
Sanya2545 <sasha2003bug@gmail.com>
2022-10-27 19:18:56 +0300

    First commit. Added own connection string. Builded and tested

---
+++ Hello_CodeFirst_MVC/Models/PicturesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Hello_CodeFirst_MVC.Models
+{
+    public class PicturesDbContext : DbContext
+    {
+        public PicturesDbContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Description> Descriptions { get; set; }
+    }
+}
